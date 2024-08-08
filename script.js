cbbccd51df520b58a826e3ac4ae7e0d5394a6c51
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 15:04:02 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,48 @@
+console.log("Script running...");
+// Task 1: All off button
+// Select the all off button and console log to confirm you selected successfully
+
+
+// Select all the lightbulb p tags and console log the length to confirm you selected successfully
+
+
+// Add an event listener such that when the all off button is clicked, the background color of all the lightbulbs becomes black
+
+
+
+
+
+// Task 2: CSH Mode
+// Select the CSH mode button and console log to confirm you selected successfully
+// Make sure to add an ID so you can select this button
+
+
+// Select the body and console log to confirm you selected successfully
+
+
+// Add an event listener such that when your mouse is over CSH mode button, the background color of body becomes green and the text of the lightbulb tags becomes "💻"
+
+
+
+
+// Add an event listener such that when your mouse is off CSH mode button, the background color of body becomes black again and the text of the lightbulb tags goes back to "💡"
+
+
+
+
+// Task 3: On and off features
+// Add an event listener to each lightbulb such that when you click it, the background color of that p tag changes to white
+
+
+
+
+
+
+
+// Extra credit: use classList and toggle to the lightbulb class to toggle on AND off when you click each lightbulb
+
+
+
+
+
+
